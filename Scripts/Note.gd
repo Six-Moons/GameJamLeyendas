8acf4bfa79ecaaac0fa8776968b3c058df5ab921
alegayndra <alegayndra@gmail.com>
2021-04-17 21:49:02 -0500

    Arreglé tamaños

--- Scripts/Note.gd
+++ Scripts/Note.gd
@@ -1,12 +1,12 @@
 extends Area2D
 
-const TARGET_Y = 164
+const TARGET_Y = 540
 const SPAWN_Y = -16
 const DIST_TO_TARGET = TARGET_Y - SPAWN_Y
 
-const LEFT_LANE_SPAWN = Vector2(120, SPAWN_Y)
-const CENTRE_LANE_SPAWN = Vector2(160, SPAWN_Y)
-const RIGHT_LANE_SPAWN = Vector2(200, SPAWN_Y)
+const LEFT_LANE_SPAWN = Vector2(360, SPAWN_Y)
+const CENTRE_LANE_SPAWN = Vector2(525, SPAWN_Y)
+const RIGHT_LANE_SPAWN = Vector2(680, SPAWN_Y)
 
 var speed = 0
 var hit = false
@@ -19,7 +19,7 @@ func _ready():
 func _physics_process(delta):
 	if !hit:
 		position.y += speed * delta
-		if position.y > 200:
+		if position.y > 600:
 			queue_free()
 			get_parent().reset_combo()
 	else:
