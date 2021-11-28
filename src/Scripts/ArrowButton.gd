0892beba94e913acdff0229a99ddf5cbee76ddf9
alegayndra <alegayndra@gmail.com>
2021-11-28 11:10:44 -0600

    Refactoricé código de nota

--- src/Scripts/ArrowButton.gd
+++ src/Scripts/ArrowButton.gd
@@ -7,20 +7,19 @@ var current_note = null
 
 export var input = ""
 
-
 func _unhandled_input(event):
 	if event.is_action(input):
 		if event.is_action_pressed(input, false):
 			if current_note != null:
+				var pos
 				if perfect:
-					get_parent().increment_score(3)
-					current_note.destroy(3)
+					pos = 3
 				elif good:
-					get_parent().increment_score(2)
-					current_note.destroy(2)
+					pos = 2
 				elif okay:
-					get_parent().increment_score(1)
-					current_note.destroy(1)
+					pos = 1
+				get_parent().increment_score(pos)
+				current_note.destroy(pos)
 				_reset()
 			else:
 				get_parent().increment_score(0)
@@ -29,42 +28,34 @@ func _unhandled_input(event):
 		elif event.is_action_released(input):
 			$PushTimer.start()
 
-
 func _on_PerfectArea_area_entered(area):
 	if area.is_in_group("note"):
 		perfect = true
-
 
 func _on_PerfectArea_area_exited(area):
 	if area.is_in_group("note"):
 		perfect = false
 
-
 func _on_GoodArea_area_entered(area):
 	if area.is_in_group("note"):
 		good = true
 
-
 func _on_GoodArea_area_exited(area):
 	if area.is_in_group("note"):
 		good = false
-
 
 func _on_OkayArea_area_entered(area):
 	if area.is_in_group("note"):
 		okay = true
 		current_note = area
 
-
 func _on_OkayArea_area_exited(area):
 	if area.is_in_group("note"):
 		okay = false
 		current_note = null
 
-
 func _on_PushTimer_timeout():
 	frame = 0
-
 
 func _reset():
 	current_note = null
